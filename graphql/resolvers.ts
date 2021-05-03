b338f12210e18027f704fd9e7708acc89e077042
Cat <holquew@gmail.com>
2021-05-03 18:55:29 +0900

    Add delete, create movie resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,10 +1,15 @@
-import {getById, movies} from "./db";
+import {getById, movies, addMovie, deleteMovie} from "./db";
 
 const resolvers = {
     Query: {
         movies: () => movies, 
         movie: (_: any, {id}: any) => getById(id),
+    }, 
+    Mutation: {
+        addMovie: (_: any, {name, score}: any) => addMovie(name, score),
+        deleteMovie: (_: any, {id}: any) => deleteMovie(id),
     }
+
 };
 
 export default resolvers;
