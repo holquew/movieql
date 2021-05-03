3aa30c7c2fd45fbd9e6b7b24fe0b039dcb6750a1
Cat <holquew@gmail.com>
2021-05-03 19:48:27 +0900

    Add port configuration for deployment

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { GraphQLServer  } from "graphql-yoga";
 import resolvers from "./graphql/resolvers"
 
+const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({
     typeDefs: "graphql/schema.graphql",
     resolvers
 }); 
 
-server.start(() => console.log("[HQ] GraphQl Server Running"))
+server.start({port: PORT}, () => console.log(`[HQ] GraphQl Server Running on http://localhost:${PORT}`))
 
 
